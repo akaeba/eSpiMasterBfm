e27f28324fe024849e24186e0373475f92b992ec
Andreas Kaeberlein <andreas.kaeberlein@web.de>
2020-03-08 17:05:01 +0100

    add header of eSpiMasterBfm.vhd

---
+++ bfm/eSpiMasterBfm.vhd
@@ -0,0 +1,21 @@
+--************************************************************************
+-- @author:  	Andreas Kaeberlein
+-- @copyright:	Copyright 2020
+-- @credits: 	AKAE
+--
+-- @license:  	GPLv3
+-- @maintainer:	Andreas Kaeberlein
+-- @email:		andreas.kaeberlein@web.de
+--
+-- @file:       eSpiMasterBfm.vhd
+-- @note:       VHDL'93
+-- @date:   	2020-01-04
+--
+-- @see:		
+-- @brief:      bus functional model for enhanced SPI (eSPI)
+--				provides function to interact with an eSPI 
+--				slave
+--************************************************************************
+
+
+
