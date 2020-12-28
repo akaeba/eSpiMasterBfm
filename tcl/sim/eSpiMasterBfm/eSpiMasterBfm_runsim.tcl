2366abe9632797b3e10efd0ba3a3f6aeb382e7c8
Andreas Kaeberlein <andreas.kaeberlein@web.de>
2020-12-28 16:51:41 +0100

    tabs by blanks

--- tcl/sim/eSpiMasterBfm/eSpiMasterBfm_runsim.tcl
+++ tcl/sim/eSpiMasterBfm/eSpiMasterBfm_runsim.tcl
@@ -1,15 +1,15 @@
 ##************************************************************************
-## @author:  	Andreas Kaeberlein
-## @copyright:	Copyright 2020
-## @credits: 	AKAE
+## @author:     Andreas Kaeberlein
+## @copyright:  Copyright 2020
+## @credits:    AKAE
 ##
-## @license:  	BSDv3
-## @maintainer:	Andreas Kaeberlein
-## @email:		andreas.kaeberlein@web.de
+## @license:    BSDv3
+## @maintainer: Andreas Kaeberlein
+## @email:      andreas.kaeberlein@web.de
 ##
 ## @file:       eSpiMasterBfm_runsim.tcl
 ## @note:       VHDL'93
-## @date:   	2020-04-24
+## @date:       2020-04-24
 ##
 ## @brief:      starts simulation
 ##************************************************************************
@@ -17,11 +17,11 @@
 
 
 # start simulation, disable optimization
-vsim -novopt -t 1ps work.eSpiMasterBfm_tb
-#vsim -novopt -gDO_ALL_TEST=true -t 1ps work.eSpiMasterBfm_tb
+#vsim -novopt -t 1ps work.eSpiMasterBfm_tb
+vsim -novopt -gDO_ALL_TEST=true -t 1ps work.eSpiMasterBfm_tb
 
 # load Waveform
 do "../tcl/sim/eSpiMasterBfm/eSpiMasterBfm_waveform.do"
 
 # sim until finish
-#run 3.5 ms
+run 160 us
